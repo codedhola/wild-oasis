fe526339f92f35ebf7dc407d3a3a0141a6cea913
codedhola <codedhola2000@gmail.com>
2024-04-22 23:01:29 +0100

    fix: previous bug fix included

--- src/features/settings/UpdateSettingsForm.tsx
+++ src/features/settings/UpdateSettingsForm.tsx
@@ -1,21 +1,53 @@
 import Form from '../../ui/Form';
 import FormRow from '../../ui/FormRow';
 import Input from '../../ui/Input';
+import Spinner from '../../ui/Spinner';
+import { useSettings } from './useSettings';
+import { useUpdateSetting } from './useUpdateSettings';
 
 function UpdateSettingsForm() {
+  const { isLoading, settings, error } = useSettings()
+  
+  if(isLoading) return <Spinner />
+
+  const { min_booking_length, max_booking_length, max_guess_per_booking, breakfast_price } = settings;
+
+  const { isUpdating, updateSetting } = useUpdateSetting();
+
+  if (isLoading) return <Spinner />;
+
+  function handleUpdate(e: any, field: any) {
+    const { value } = e.target;
+
+    if (!value) return;
+    updateSetting({ [field]: value });
+  }
+
   return (
     <Form>
       <FormRow label='Minimum nights/booking'>
-        <Input type='number' id='min-nights' />
+        <Input type='number' id='min-nights' 
+        defaultValue={min_booking_length} 
+        onBlur={e => handleUpdate(e, 'min_booking_length')}
+        />
       </FormRow>
       <FormRow label='Maximum nights/booking'>
-        <Input type='number' id='max-nights' />
+        <Input type='number' id='max-nights'
+        defaultValue={max_booking_length}
+        onBlur={e => handleUpdate(e, 'max_booking_length')}
+        />
       </FormRow>
       <FormRow label='Maximum guests/booking'>
-        <Input type='number' id='max-guests' />
+        <Input type='number' id='max-guests'
+        defaultValue={max_guess_per_booking}
+        onBlur={e => handleUpdate(e, 'max_guess_per_booking')}
+        />
       </FormRow>
       <FormRow label='Breakfast price'>
-        <Input type='number' id='breakfast-price' />
+        <Input type='number' id='breakfast-price'
+        defaultValue={breakfast_price}
+        onBlur={e => handleUpdate(e, 'breakfast_price')}
+         />
       </FormRow>
     </Form>
   );
--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,7 +1,13 @@
+import UpdateSettingsForm from "../features/settings/UpdateSettingsForm";
 import Heading from "../ui/Heading";
 
 function Settings() {
-  return <Heading as="h1">Update hotel settings</Heading>;
+  return (
+  <section>
+    <Heading as="h1">Update hotel settings</Heading>
+    <UpdateSettingsForm />
+  </section>
+  );
 }
 
 export default Settings;
