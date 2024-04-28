db19d13685164a9ec069c79f80bf752d55404d1e
codedhola <codedhola2000@gmail.com>
2024-04-28 05:45:04 +0100

    feat(core): built filter and sorting for bookings

--- src/features/bookings/useBookings.ts
+++ src/features/bookings/useBookings.ts
@@ -1,11 +1,29 @@
 import { useQuery } from "@tanstack/react-query";
 import { getAllBookings } from "../../services/apiBookings";
+import { useSearchParams } from "react-router-dom";
 
 
 export function useAllBookings(){
+  const [searchParams] = useSearchParams();
+
+  // FILTER
+  const filterValue = searchParams.get("status");
+  const filter =
+    !filterValue || filterValue === "all"
+      ? null
+      : { field: "status", value: filterValue };
+  // { field: "totalPrice", value: 5000, method: "gte" };
+
+  // SORT
+  const sortByRaw = searchParams.get("sortBy") || "start_date-desc";
+  const [field, direction] = sortByRaw.split("-");
+  const sortBy = { field, direction };
+
+  // PAGINATION
+  const page = !searchParams.get("page") ? 1 : Number(searchParams.get("page"));
   const { isLoading, error, data: bookings } = useQuery({
-    queryKey: ["bookings"],
-    queryFn: getAllBookings
+    queryKey: ["bookings", filter, sortBy, page],
+    queryFn: () => getAllBookings({ filter, sortBy, page})
   })
 
   return { bookings, isLoading, error }
--- src/ui/Tag.tsx
+++ src/ui/Tag.tsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-const Tag = styled.span`
+const Tag = styled.span<any>`
   width: fit-content;
   text-transform: uppercase;
   font-size: 1.1rem;
@@ -9,7 +9,7 @@ const Tag = styled.span`
   border-radius: 100px;
 
   /* Make these dynamic, based on the received prop */
-  color: var(--color-${(props) => props.type}-700);
+  color: var(--color-${(props: any) => props.type}-700);
   background-color: var(--color-${(props) => props.type}-100);
 `;
 
