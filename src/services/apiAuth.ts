fccfc8bd89341f814c8d46641da594ecf0ef9127
codedhola <codedhola2000@gmail.com>
2024-06-14 00:21:17 +0100

    feat: added signup

--- src/services/apiAuth.ts
+++ src/services/apiAuth.ts
@@ -1,6 +1,24 @@
 import toast from "react-hot-toast";
 import supabase from "./supabase";
 
+export async function signup({ fullName, email, password }: any) {
+  const { data, error } = await supabase.auth.signUp({
+    email,
+    password,
+    options: {
+      data: {
+        fullName,
+        avatar: "",
+      },
+    },
+  });
+
+  if (error) throw new Error(error.message);
+
+  return data;
+}
+
+
 export async function Login({ email, password}: any){
  
 let { data, error } = await supabase.auth.signInWithPassword({
